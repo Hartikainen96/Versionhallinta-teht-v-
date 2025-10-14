1b31417b3377c6912277e954b9898d0f17255b86
Tuomas Hartikainen <tuomas.hartikainen1996@hotmail.fi>
2025-10-15 00:49:31 +0300

    Create App.java

---
+++ App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+      Scanner in = new Scanner(System.in);
+   String oikeaarvaus = "Teemu";
+   String kayttajanarvaus = "";
+   String lopetus = "stop";
+   int arvaukset = 0;
+
+   do {
+    System.out.println("Guess my name (type stop to exit)");
+    
+    kayttajanarvaus = in.nextLine();
+
+    if (kayttajanarvaus.equals(lopetus))
+    {
+      System.out.println("You guessed " + arvaukset +  " times.");
+      break;
+    }
+    arvaukset++;
+
+    if (kayttajanarvaus.equals(oikeaarvaus))
+    {
+      System.out.println("Congratulations!");
+      System.out.println("You guessed " + arvaukset + " times. ");
+      break;
+    }
+   }
+   while (true);
+    }
+  }
